187791c8d1d54b2e4787458fceda313e1154c618
ramidlr <ramidlr@ramidlrs-MacBook-Pro.local>
2023-08-30 00:33:42 +0200

    login form clean

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -8,8 +8,8 @@ import { AuthService } from '../../auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  public emailControl = new FormControl('admin@admin.com', [Validators.required, Validators.email]);
-  public passwordControl = new FormControl('12345678', Validators.required);
+  public emailControl = new FormControl('', [Validators.required, Validators.email]);
+  public passwordControl = new FormControl('', Validators.required);
 
   public loginForm = new FormGroup({
     email: this.emailControl,
